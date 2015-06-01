5ed81072e23b6b08bb64662ccbcdc2c173f07de9
МаксимБагашев <maxim.bagashev@altarix.ru>
2015-06-01 11:20:28 +0300

    Added Animation (cherry picked from commit 52449db)

--- src/main/java/com/seriouscompany/supagraphview/viewandcontroller/GraphView.java
+++ src/main/java/com/seriouscompany/supagraphview/viewandcontroller/GraphView.java
@@ -6,14 +6,51 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Igor on 12.04.2015.
  */
 public class GraphView extends JPanel {
 
+    private List<int[]> prevCoords = new ArrayList<>();
+    private List<int[]> currCoords = new ArrayList<>();
+    private float animPosition;
+    private ScheduledThreadPoolExecutor executor;
+
     public void setGraph(Graph graph) {
         this.graph = graph;
+
+        if(prevCoords.size() == graph.graphCoordinates.size()) {
+
+            List<int[]> interpCoords = getGraphPoints();
+
+            prevCoords.clear();
+            prevCoords.addAll(interpCoords);
+            currCoords.clear();
+            currCoords.addAll(graph.graphCoordinates);
+
+            animPosition = 0;
+
+            if(executor != null) {
+                executor.shutdownNow();
+            }
+
+            executor = new ScheduledThreadPoolExecutor(1);
+            executor.scheduleAtFixedRate(new Runnable() {
+                @Override
+                public void run() {
+                    repaint();
+                }
+            }, 0, 16, TimeUnit.MILLISECONDS);
+        } else {
+            prevCoords.clear();
+            currCoords.clear();
+
+            prevCoords.addAll(graph.graphCoordinates);
+            currCoords.addAll(graph.graphCoordinates);
+        }
     }
 
     private Graph graph = null;
@@ -41,7 +78,7 @@ public class GraphView extends JPanel {
             graph.maxY = getHeight() - 30;
             Graphics2D g2D = (Graphics2D) g;
             g2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-            List<int[]> points = graph.graphCoordinates;
+            List<int[]> points = getGraphPoints();
             int[][] graphMatrix = graph.graph;
             if (points != null && !points.isEmpty()) {
                 for (int i = 0; i < graphMatrix.length; i++) {
@@ -64,6 +101,37 @@ public class GraphView extends JPanel {
                     g2D.drawString(String.valueOf(i), first[0] + 5, first[1] + 5);
                 }
             }
+
+            animPosition += 0.02f;
+
+            if(animPosition > 1) {
+                animPosition = 1;
+
+                if(executor != null) {
+                    executor.shutdownNow();
+                }
+            }
         }
+    }
+
+    private List<int[]> getGraphPoints() {
+        List<int[]> list = new ArrayList<>();
+        int[] prev, curr;
+        float anim = (float) (0.5f - Math.cos((animPosition * (Math.PI / 2)) * 2) / 2);
+
+        for(int i = 0; i < prevCoords.size(); i++) {
+            prev = prevCoords.get(i);
+            curr = currCoords.get(i);
+
+            list.add(new int[] {
+                lerp(prev[0], curr[0], anim),
+                lerp(prev[1], curr[1], anim)
+            });
+        }
+
+        return list;
+    }
+    private int lerp(int from, int to, float t) {
+        return (int) (from + (to - from) * t);
     }
 }
